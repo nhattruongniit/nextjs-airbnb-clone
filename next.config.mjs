9bb9510369e6f9f06c9a8fcaad9547ad0b0a5d9c
tonynguyen <tonyjsfreelancer@gmail.com>
2024-12-08 23:59:43 +0700

    down next v13 to deploy vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: [
-      'avatars.githubusercontent.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'res.cloudinary.com',
     ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '**',
-      },
-    ],
-  }
+  },
 };
 
 export default nextConfig;
